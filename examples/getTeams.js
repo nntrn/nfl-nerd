3cc613d0696b1a10a65bfb05f644e5c03d952fb5
annie tran <nntrn@me.com>
2020-10-14 18:35:12 -0500

    Remove old examples

--- examples/getTeams.js
+++
@@ -1,5 +0,0 @@
-const { getTeams } = require('../src/getTeams')
-const { createAndWrite } = require('../src/utils')
-
-getTeams()
-  .then(t => createAndWrite('./tmp2/teams-cache.json', JSON.stringify(t, null, 2)))
